2d82ce55ec733b206b890cfb9112c947e7e66caf
kmangoes <sp24lights@gmail.com>
2025-12-27 13:53:33 -0500

    init commit

---
+++ project/high-fidelity-prototype/demo/src/main/java/com/example/demo/LoreBuddyPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoreBuddyPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoreBuddyPrototypeApplication.class, args);
+	}
+
+}
